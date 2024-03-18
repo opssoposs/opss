7b58417d754d275ff5fa71be6874b5e9bcd475e1
opssoposs <opssoposs@naver.com>
2024-03-18 15:59:26 +0900

    dd

--- first/index.js
+++ first/index.js
@@ -1,13 +1,15 @@
 const canvas = document.querySelector("canvas");
-const c = canvas.getContext("2d"); 
+const c = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
 
+const gravity = 0.2;
+
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor( { position, velocity } )
+    constructor( { position, velocity })
     {
         this.position = position;
 
@@ -25,7 +27,18 @@ class Sprite {
     update() {
         this.draw();
 
-        this.position.y += 10;
+        this.position.y += this.velocity.y;
+
+        this.position.x += this.velocity.x;
+
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        }
+        else {
+            this.velocity.y += gravity;
+        }
+            
+
     }
 }
 
@@ -35,26 +48,32 @@ const player = new Sprite( {
         y :0,
     },
     velocity: {
-        x :0,
-        y :0,
+        x:0,
+        y:10,
     }
-   
-});
-const enemy = new Sprite({
+})
+
+const enemy = new Sprite( {
     position: {
         x :400,
         y :100,
     },
     velocity: {
-        x :400,
-        y :100,
+        x:0,
+        y:0,
     }
 });
 
 console.log(player);
 
-// Enemy.draw();
+const keys = {
+    d: {
+        pressed : false,
+    }
+}
+
 // player.draw();
+// enemy.draw();
 
 function animate() {
     window.requestAnimationFrame(animate);
@@ -64,6 +83,33 @@ function animate() {
 
     player.update();
     enemy.update();
+
+    player.velocity.x = 0;
 }
 
-anumate();
+animate();
+
+window.addEventListener("keydown", (event) => {
+    console.log(event.key);
+
+    switch(event.key) {
+        case"d":
+            player.velocity.x = 1;
+            break;
+        case"a":
+            player.velocity.x = -1;
+            break;
+    }
+})
+window.addEventListener("keyup", (event) => {
+    console.log(event.key);
+
+    switch(event.key) {
+        case"d":
+        player.velocity.x = 0;
+            break;
+        case"d":
+            player.velocity.x = -0;
+            break;
+    }
+})
