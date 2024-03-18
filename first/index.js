0c8617255d2598cfbfea4174fd5807aa6f80569b
opssoposs <opssoposs@naver.com>
2024-03-18 14:37:23 +0900

    js

--- first/index.js
+++ first/index.js
@@ -1,2 +1,15 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d"); 
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor(position)
+    {
+        this.position.position;
+    }
+}
+const player = new Sprite
